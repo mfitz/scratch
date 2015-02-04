e4bd86ffa1131aa6b0c958b1f845cd1767a5fddf
Michael Fitzmaurice <michael@michaelfitzmaurice.com>
2015-02-04 17:46:21 +0000

    Added gitignore file

--- dnn-parse/src/main/java/com/michaelfitzmaurice/dnn/DnnMenuParser.java
+++ dnn-parse/src/main/java/com/michaelfitzmaurice/dnn/DnnMenuParser.java
@@ -1,6 +1,7 @@
 package com.michaelfitzmaurice.dnn;
 
 import java.io.File;
+import java.io.PrintStream;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
@@ -22,22 +23,29 @@ public class DnnMenuParser {
 		DefaultMutableTreeNode child1 = new DefaultMutableTreeNode("Child 1");
 		root.add(child1);
 		root.add( new DefaultMutableTreeNode("Child 2") );
-		child1.add( new DefaultMutableTreeNode("Grandchild1") );
+		DefaultMutableTreeNode grandChild1 = new DefaultMutableTreeNode("Grandchild 1");
+		grandChild1.add( new DefaultMutableTreeNode("GreatGrandchild2") );
+		child1.add(grandChild1);
 		child1.add( new DefaultMutableTreeNode("Grandchild2") );
 		
-		printTree(root);
+		printTree(root, System.out);
 	}
 	
-	public static void printTree(DefaultMutableTreeNode node) {
+	public static void printTree(DefaultMutableTreeNode node, PrintStream stream) {
 		
 		for (int i = 0; i < node.getChildCount(); i++) {
 			DefaultMutableTreeNode child = 
 				(DefaultMutableTreeNode)node.getChildAt(i);
-	        for (int j = 0; j < child.getLevel(); j++) {
-	            System.out.print("  ");
-            }
-			System.out.println( "|---- " + child.toString() );
-			printTree(child);
+			stream.print(" ");
+			int level = child.getLevel();
+			if (level > 1) {
+		        for (int j = 0; j < level - 2; j++) {
+		            stream.print("     ");
+	            }
+				stream.print("|---- ");
+			}
+			stream.println( child.toString() );
+			printTree(child, stream);
         }
 	}
 
