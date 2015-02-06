c4b9f0282ee6cc535a87ff08afce5006cdcb3c67
Michael Fitzmaurice <michael@michaelfitzmaurice.com>
2015-02-06 03:55:39 +0000

    DNN menu parser - added a couple of comments

--- dnn-parse/src/main/java/com/michaelfitzmaurice/dnn/DnnMenuParser.java
+++ dnn-parse/src/main/java/com/michaelfitzmaurice/dnn/DnnMenuParser.java
@@ -14,8 +14,8 @@ import javax.swing.tree.DefaultMutableTreeNode;
  * Quick and dirty class to parse the sitemap from a DotNetNuke generated
  * JavaScript menu and dump it to the console as a tree represented in 
  * plain text. The DNN generated JavaScript needs to be cleaned up 
- * slightly before being fed to this application, specifically (in this order),
- * take the web page that contains the site map definition and:
+ * slightly before being fed to this application, specifically, take the web 
+ * page that contains the site map definition and (in this order):
  * <ol>
  *   <li>Remove everything that is not between the <SCRIPT language="javascript"> tags
  *   <li>All instances of the string "&nbsp;" should be removed (replaced with nothing)
@@ -28,7 +28,6 @@ import javax.swing.tree.DefaultMutableTreeNode;
 public class DnnMenuParser {
 	
 	private static final String NODE_MARKER_TEXT = "=new SPJSXMLNode";
-	
 	private static final String ROOT_NODE_KEY = "m_oSPNode['-1']";
 
 	public static void main(String[] args) throws Exception {
@@ -52,7 +51,8 @@ public class DnnMenuParser {
 			String[] tokens = line.split(NODE_MARKER_TEXT);
 			if (tokens != null && tokens.length > 1) {
 				String nodeProperties = tokens[1];
-				nodeProperties = nodeProperties.substring(1, nodeProperties.length() );
+				nodeProperties = 
+						nodeProperties.substring(1, nodeProperties.length() );
 				tokens = nodeProperties.split(",");
 				if (tokens != null && tokens.length > 3) {
 					String nodeType = tokens[0].trim();
@@ -60,9 +60,9 @@ public class DnnMenuParser {
 					String parentId = tokens[2].trim();
 					String nodeValue = tokens[3].trim();
 					DefaultMutableTreeNode treeNode = 
-							new DefaultMutableTreeNode(nodeValue);
+						new DefaultMutableTreeNode(nodeValue);
 					if ( nodeType.equals("'root'") ) {
-						nodeMap.put( ROOT_NODE_KEY, treeNode );
+						nodeMap.put(ROOT_NODE_KEY, treeNode);
 					} else {
 						nodeId = "m_oSPNode[" + nodeId + "]";
 						DefaultMutableTreeNode parent = nodeMap.get(parentId);
@@ -80,15 +80,21 @@ public class DnnMenuParser {
 		return nodeMap.get(ROOT_NODE_KEY);
 	}
 	
-	public static void printTree(DefaultMutableTreeNode node, PrintStream stream) {
+	public static void printTree(DefaultMutableTreeNode node, 
+								PrintStream stream) {
 		
 		for (int i = 0; i < node.getChildCount(); i++) {
 			DefaultMutableTreeNode child = 
 				(DefaultMutableTreeNode)node.getChildAt(i);
 			int level = child.getLevel();
+			// root node is level 0, and is a special node containing
+			// no actual menu item. we want children of the root (top level
+			// menu items) to be left justified, then appropriate indentation 
+			// for each subsequent generation
 			if (level > 1) {
 				stream.print(" ");
 		        for (int j = 0; j < level - 2; j++) {
+		        	// only level 3 and greater needs leading whitespace
 		            stream.print("     ");
 	            }
 		        stream.print("|---- ");
